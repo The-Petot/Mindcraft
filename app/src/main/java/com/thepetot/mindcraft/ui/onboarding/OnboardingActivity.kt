236b0cb0d78f6aae667936537dbe48f7f12ccf50
ts0ra <gaming.heroes354@gmail.com>
2024-11-22 21:46:38 +0700

    change(onboarding): enable viewBinding

--- app/src/main/java/com/thepetot/mindcraft/ui/onboarding/OnboardingActivity.kt
+++ app/src/main/java/com/thepetot/mindcraft/ui/onboarding/OnboardingActivity.kt
@@ -7,17 +7,24 @@ import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.thepetot.mindcraft.R
+import com.thepetot.mindcraft.databinding.ActivityOnboardingBinding
 
 class OnboardingActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityOnboardingBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         installSplashScreen()
-        setContentView(R.layout.activity_onboarding)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.onboarding)) { v, insets ->
+        binding = ActivityOnboardingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
     }
 }
